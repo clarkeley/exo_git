bd16f7ccec8ce524547b1920f882d57ec91a5538
Emm_A <clarke_ley@live.fr>
2016-03-10 15:39:07 +0100

    Modifs dans la balise <h3>

--- menu.php
+++ menu.php
@@ -1,1 +1,11 @@
-
+    <!-- Le menu -->
+    <nav id="menu">        
+        <div class="element_menu">
+            <h3>Plus d'infos</h3>
+            <ul>
+                <li><a href="page1.html">Lien1</a></li>
+                <li><a href="page2.html">Lien2</a></li>
+                <li><a href="page3.html">Lien3</a></li>
+            </ul>
+        </div>    
+    </nav>
