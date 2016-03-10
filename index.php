ffccbb4f268ecf06f04917c926a9f1edd70d9152
Emm_A <clarke_ley@live.fr>
2016-03-10 15:25:05 +0100

    Création du code de la page

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
 
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon Site Web</title>
+    </head>
+
+    <body>
+
+	<!-- L'en-tête -->
+    <header>
+
+    </header>
+
+	<?php include ("menu.php"); ?>
+
+    <!-- Le corps -->
+    <div id="corps">
+        <h1>Mes passions</h1>
+        <p>
+            Je vais vous présenter sur ce site tout ce qui m'intéresse<br />
+			Je cherche encore le design de mon site web :P
+        </p>
+    </div>
+
+    <!-- Le pied de page -->
+	<footer id="pied_de_page">
+        <p>Copyright moi, tous droits réservés</p>
+    </footer>
+
+    </body>
+
+</html>
